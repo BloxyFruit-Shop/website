bc094d64a9cf9a59d8c601a733b911d41beba6a3
Axel Castaño <axelcastano@proton.me>
2025-04-03 04:20:29 -0300

    Added check for user using his own referral code

--- src/routes/api/webhooks/order/+server.js
+++ src/routes/api/webhooks/order/+server.js
@@ -18,41 +18,55 @@ export async function POST({ request }) {
 
   const order = JSON.parse(body);
 
+  // Check that the note has an affiliate code attached
   if (!order?.note?.startsWith('affiliate-')) {
     console.log('No affiliate');
-    return new Response('No effect', { status: 200 });
-  }
-
-  const bought = order.line_items.reduce((acc, item) => {
-    const id = String(item.variant_id);
-    const amount = item.quantity;
-
-    acc[id] = amount;
-    return acc;
-  }, {});
-
-  const productIds = Object.keys(bought);
-  if (!productIds.length) {
-    console.log('No productIds');
     return new Response('No effect', { status: 200 });
   }
 
   const affiliate = order.note.split('affiliate-')[1];
   const user = await users.findOne({ referralCode: affiliate }).lean();
 
+  // Check if the user exists and is not using his own code
   if (!user) {
     console.log('User not found', affiliate);
     return new Response('Invalid affiliate code', { status: 200 });
   }
 
+  const buyerEmail = order.contact_email;
+  
+  if (user.email === buyerEmail) {
+    console.log('User used his own code:', affiliate);
+    return new Response('No effect', { status: 200 });
+  }
+
+  // Get the bought items ids and their quantities
+  const bought = order.line_items.reduce((acc, item) => {
+    const id = String(item.variant_id);
+    const amount = item.quantity;
+
+    acc[id] = amount;
+    return acc;
+  }, {}); // {'id' : quantity} => Object{ '12345678' : 1, '12345679' : 2 }
+
+  // If no products, return no effect
+  const productIds = Object.keys(bought);
+  if (!productIds.length) {
+    console.log('No productIds');
+    return new Response('No effect', { status: 200 });
+  }
+
+  // TODO: Update the way the robuxAmount is calculated
   const items = await products
     .find({ productId: { $in: productIds } })
     .lean();
   const robuxAmount = items.reduce((acc, item) => {
     const amount = bought[item.productId] ?? 0;
-    return acc + (item?.robux * amount ?? 0);
+    const cost = item?.robux ?? 0
+    return acc + (cost * amount);
   }, 0);
 
+  // Update the user's robux balance
   await users.updateOne(
     { _id: user._id },
     { $inc: { robux: robuxAmount } }
