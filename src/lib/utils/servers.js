53b08f0326223efa79fa635eaaa69686a34677d5
Axel Castaño <axelcastano@proton.me>
2025-10-26 00:22:24 -0300

    Fixed broken server invite

--- src/lib/utils/servers.js
+++ src/lib/utils/servers.js
@@ -8,5 +8,5 @@ export default {
   'king-legacy': 'https://discord.gg/8sjBPzzxwA',
   'blue-lock': 'https://discord.gg/u53cFFR95H',
   'steal-brainrot': 'https://discord.gg/xhEXNBkS7w',
-  'plants-vs-brainrot': 'https://discord.gg/u4Pc84kT'
+  'plants-vs-brainrot': 'https://discord.gg/HVPt5ceHan'
 };
